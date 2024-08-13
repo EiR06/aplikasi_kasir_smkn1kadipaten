97c84b6e4eee20bc108ea3020ff1c7848baaaf93
EiR <152456176+EiR06@users.noreply.github.com>
2024-08-13 19:23:12 +0700

    Update Tampilan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,29 +4,29 @@
 <div class="login-box">
 
     <!-- /.login-logo -->
-    <div class="login-box-body">
+    <div class="login-box-body animated fadeInDown" style="background-color: #f7f9fc; padding: 30px; border-radius: 10px; box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);">
         <div class="login-logo">
             <a href="{{ url('/') }}">
-                <img src="{{ url($setting->path_logo) }}" alt="logo.png" width="100">
+                <img src="{{ url($setting->path_logo) }}" alt="logo.png" width="100" class="animated bounceIn" style="margin-bottom: 20px;">
             </a>
         </div>
 
         <form action="{{ route('login') }}" method="post" class="form-login">
             @csrf
             <div class="form-group has-feedback @error('email') has-error @enderror">
-                <input type="email" name="email" class="form-control" placeholder="Email" required value="{{ old('email') }}" autofocus>
-                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                <input type="email" name="email" class="form-control" placeholder="Email" required value="{{ old('email') }}" autofocus style="border-radius: 5px; padding: 10px;">
+                <span class="glyphicon glyphicon-envelope form-control-feedback animated swing infinite" style="color: #007bff;"></span>
                 @error('email')
-                    <span class="help-block">Email/Password Wrong</span>
+                    <span class="help-block animated flash" style="color: red;">Email/Password Wrong</span>
                 @else
-                <span class="help-block with-errors"></span>
+                    <span class="help-block with-errors"></span>
                 @enderror
             </div>
             <div class="form-group has-feedback @error('password') has-error @enderror">
-                <input type="password" name="password" class="form-control" placeholder="Password" required>
-                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                <input type="password" name="password" class="form-control" placeholder="Password" required style="border-radius: 5px; padding: 10px;">
+                <span class="glyphicon glyphicon-lock form-control-feedback animated swing infinite" style="color: #007bff;"></span>
                 @error('password')
-                    <span class="help-block">Email/Password Wrong</span>
+                    <span class="help-block animated flash" style="color: red;">Email/Password Wrong</span>
                 @else
                     <span class="help-block with-errors"></span>
                 @enderror
@@ -35,13 +35,13 @@
                 <div class="col-xs-8">
                     <div class="checkbox icheck">
                         <label>
-                            <input type="checkbox"> Remember Me
+                            <input type="checkbox" style="margin-right: 5px;"> Remember Me
                         </label>
                     </div>
                 </div>
                 <!-- /.col -->
                 <div class="col-xs-4">
-                    <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                    <button type="submit" class="btn btn-primary btn-block btn-flat animated pulse" style="background-color: #007bff; border: none; border-radius: 5px;">Sign In</button>
                 </div>
                 <!-- /.col -->
             </div>
