a688294267f9a12d97591c6cefce17c2b105ef27
EiR <152456176+EiR06@users.noreply.github.com>
2024-08-13 19:47:40 +0700

    Update Tampilan

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -26,12 +26,12 @@
             <li class="header">DATA</li>
             <li>
                 <a href="{{ route('kategori.index') }}">
-                    <i class="fa fa-cube"></i> <span>Kategori</span>
+                    <i class="fa fa-th-large"></i> <span>Kategori</span>
                 </a>
             </li>
             <li>
                 <a href="{{ route('produk.index') }}">
-                    <i class="fa fa-cubes"></i> <span>Produk</span>
+                    <i class="fa fa-tags"></i> <span>Produk</span>
                 </a>
             </li>
             <li>
@@ -41,7 +41,7 @@
             </li>
             <li>
                 <a href="{{ route('supplier.index') }}">
-                    <i class="fa fa-truck"></i> <span>Supplier</span>
+                    <i class="fa fa-briefcase"></i> <span>Supplier</span>
                 </a>
             </li>
             <li class="header">TRANSAKSI</li>
