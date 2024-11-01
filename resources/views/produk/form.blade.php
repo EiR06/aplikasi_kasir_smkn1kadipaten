ea4ca1e7b2389193e96941120e82feedb1baeed0
EiR <152456176+EiR06@users.noreply.github.com>
2024-11-01 21:49:23 +0700

    Update fForm Produk

--- resources/views/produk/form.blade.php
+++ resources/views/produk/form.blade.php
@@ -5,21 +5,22 @@
             @method('post')
 
             <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                            aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title"></h4>
+                <div class="modal-header  bg-primary text-white">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                       <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title">Form Produk</h4>
                 </div>
                 <div class="modal-body">
                     <div class="form-group row">
-                        <label for="nama_produk" class="col-lg-2 col-lg-offset-1 control-label">Nama</label>
+                        <label for="nama_produk" class="col-lg-2 col-lg-offset-1 control-label">Nama <i class="fa fa-tag"></i></label>
                         <div class="col-lg-6">
                             <input type="text" name="nama_produk" id="nama_produk" class="form-control" required autofocus>
                             <span class="help-block with-errors"></span>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="id_kategori" class="col-lg-2 col-lg-offset-1 control-label">Kategori</label>
+                        <label for="id_kategori" class="col-lg-2 col-lg-offset-1 control-label">Kategori <i class="fa fa-list"></i></label>
                         <div class="col-lg-6">
                             <select name="id_kategori" id="id_kategori" class="form-control" required>
                                 <option value="">Pilih Kategori</option>
@@ -31,14 +32,14 @@
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="merk" class="col-lg-2 col-lg-offset-1 control-label">Merk</label>
+                        <label for="merk" class="col-lg-2 col-lg-offset-1 control-label">Merk <i class="fa fa-cube"></i></label>
                         <div class="col-lg-6">
                             <input type="text" name="merk" id="merk" class="form-control">
                             <span class="help-block with-errors"></span>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="harga_beli" class="col-lg-2 col-lg-offset-1 control-label">Harga Beli</label>
+                        <label for="harga_beli" class="col-lg-2 col-lg-offset-1 control-label">Harga Beli <i class="fa fa-money"></i></label>
                         <div class="col-lg-6">
                             <input type="number" name="harga_beli" id="harga_beli" class="form-control" required>
                             <span class="help-block with-errors"></span>
@@ -52,14 +53,14 @@
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="diskon" class="col-lg-2 col-lg-offset-1 control-label">Diskon</label>
+                        <label for="diskon" class="col-lg-2 col-lg-offset-1 control-label">Diskon (%)</label>
                         <div class="col-lg-6">
                             <input type="number" name="diskon" id="diskon" class="form-control" value="0">
                             <span class="help-block with-errors"></span>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="stok" class="col-lg-2 col-lg-offset-1 control-label">Stok</label>
+                        <label for="stok" class="col-lg-2 col-lg-offset-1 control-label">Stok <i class="fa fa-box"></i></label>
                         <div class="col-lg-6">
                             <input type="number" name="stok" id="stok" class="form-control" required value="0">
                             <span class="help-block with-errors"></span>
